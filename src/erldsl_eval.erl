a2456c1d47da18f6c5213a6e10934a24b4b349b9
Ely Arzhannikov <iarzhannikov@gmail.com>
2012-03-07 20:09:14 +0600

    eval ops

--- src/erldsl_eval.erl
+++ src/erldsl_eval.erl
@@ -11,8 +11,11 @@ new_context() ->
 
 
 eval(Context, [Exp | Exps]) ->
-	{Context1, _} = eval_exp(Context, Exp),
-	eval(Context1, Exps);
+	{Context1, Result} = eval_exp(Context, Exp),
+	case Exps of
+		[] -> {Context1, Result};
+		_ -> eval(Context1, Exps)
+	end;
 
 eval(Context, Exp) ->
 	eval_exp(Context, Exp).
@@ -31,10 +34,29 @@ eval_exp(Context, {var, Line, {'NAME', _Line2, Name}}) ->
 		false ->
 			exit({unknown_variable, Name, Line})
 	end;
+eval_exp(Context, {op, _Line, Op, LeftExp, RightExp}) ->
+	{Context1, RightResult} = eval_exp(Context, RightExp),
+	{Context2, LeftResult} = eval_exp(Context1, LeftExp),
+	{Context2, do_op(Op, LeftResult, RightResult)};
+
+eval_exp(Context, {op, _Line, Op, RightExp}) ->
+	{Context1, RightResult} = eval_exp(Context, RightExp),
+	{Context1, do_op(Op, RightResult)};
 
 eval_exp(_Context, {Any, Line, _Oth}) ->
 	exit({unknown_statment, Any, Line}).
 
+do_op('-', Result) ->
+	-Result.
+
+do_op('-', Left, Right) ->
+	Left - Right;
+do_op('+', Left, Right) ->
+	Left + Right;
+do_op('*', Left, Right) ->
+	Left * Right;
+do_op('/', Left, Right) ->
+	Left / Right.
 
 assign_variable(Context, {'NAME', _Line, Name}, Result) ->
 	Context1 = dict:store(Name, Result, Context),
